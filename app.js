0028ca3d9d31b698f21c66e1ca963a5013d989d8
Darji Het <hetdarjibca22@gmail.com>
2025-07-02 16:00:26 +0530

    middleware

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-const express = require('express')
+const express = require('express');
+const morgan = require('morgan');
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send("Hello World")
+app.set("view engine", "ejs");
+
+app.use(morgan('dev'))
+
+app.use((req, res, next) => {
+    console.log("This is middleware")
+
+    return next()
+})
+
+app.get('/', (req, res, next) => {
+    console.log("This is middleware");
+    return next();
+}, (req, res) => {
+    res.render('index')
 })
 
 app.get("/about", (req, res) => {
